00cc8a7d113807cbf56e1d89be0f12c4739f35f3
nsx07 <felipeaugustodluz@hotmail.com>
2023-09-28 01:16:20 -0300

    Adicionando tailwindscss.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { CSSProperties, useState } from 'react'
 import { ChatService } from './services/chat-service';
 import InputMessage from './components/InputMessage';
 import { MessageBox } from 'react-chat-elements'
@@ -8,12 +8,12 @@ const chatService = new ChatService();
 const myMessages: string[] = [];
 
 function itsMy(men:string) {
-  return myMessages.includes(men);
+  return myMessages.map(x => x.substring(x.indexOf(" "))).some(x => men === men);
 }
 
 function App() {
   let [message, setMessage] = useState<string[]>([]);
-  const [connected, setConnected] = useState(false);
+  let [connected, setConnected] = useState(false);
 
   async function chat() {
     await chatService.connectChat();
@@ -21,73 +21,104 @@ function App() {
     setConnected(true);
 
     chatService.onMessage.subscribe(x => {
-      message = [...message, x]
+      message = [...message, x]      
       setMessage(message);
     })
 
     chatService.onClose.subscribe(x => {
       setConnected(false);
-    
     })
     
   }
 
   return (
     <>
-      <div style={{height: "100vh", display: "flex", width: "100%", justifyContent: "center",}}>
-        <div style={{height: "100%", width: "50%", display: "flex", flexDirection: "column", justifyContent:"space-between"}}>
-          <div className="card" style={{display: "flex", width: "max-content", alignItems: "center"}}>
-            <button onClick={() => !connected ? chat() : void 0} disabled={connected}>
-              Connect chat
-            </button>
-            <button onClick={() => connected ? chatService.disconnectChat() : void 0} disabled={!connected}>
-              Disconnect chat
-            </button>
-            <span style={{marginLeft: '1rem'}}>
-              {connected ? "Conectado" : "Desconectado"}
-            </span>
-          </div>
+        <div style={styles.wrapper}>
+          <div style={styles.chat}>
+            <div style={styles.header}>
+              <button onClick={() => !connected ? chat() : void 0} disabled={connected}>
+                Connect chat
+              </button>
+              <button onClick={() => connected ? chatService.disconnectChat() : void 0} disabled={!connected}>
+                Disconnect chat
+              </button>
+              <span>
+                {connected ? "Conectado" : "Desconectado"}
+              </span>
+            </div>
 
-          <div className='card' style={{display: "flex", flexDirection:"column", width: "100%", gap: "0.3rem"}}>
-            {message && message.map((x) => {
-              const key = uuidv4();
+            <div style={styles.message}>
+              {message && message.map((x) => {
+                const key = uuidv4();
+                
+                return (
+                  <div key={`${key}`} >
+                    <MessageBox
+                        id={key}
+                        focus={false}
+                        titleColor={'#333'}
+                        forwarded={true}
+                        notch={true}
+                        removeButton={false}
+                        replyButton={true}
+                        status={'sent'}
+                        retracted={true}
+                        position={itsMy(x) ? "right" : "left"}
+                        type={"text"}
+                        title={x.substring(0, x.indexOf(" "))}
+                        text={x.substring(x.indexOf(" "))}
+                        date={new Date()}
+                      />
+                  </div>
+                )
+              })}
+            </div>
+
+            <div style={styles.input}>
+              <InputMessage  onEnter={(message) => {
+                myMessages.push(message)
+                chatService.sendMessage(message)
+              }}></InputMessage>
               
-              return (
-                <div key={`${key}`} >
-                  <MessageBox
-                      id={key}
-                      focus={true}
-                      titleColor={'#333'}
-                      forwarded={true}
-                      notch={true}
-                      removeButton={false}
-                      replyButton={true}
-                      status={'waiting'}
-                      retracted={true}
-                      position={itsMy(x) ? "right" : "left"}
-                      type={"text"}
-                      title={""}
-                      text={x}
-                      date={new Date()}
-                    />
-                </div>
-              )
-            })}
-          </div>
-
-          <div className="card">
-            <InputMessage  onEnter={(message) => {
-              myMessages.push(message)
-              chatService.sendMessage(message)
-            }}></InputMessage>
+            </div>
             
           </div>
         </div>
-
-      </div>
-      
     </>
   )
 }
 
 export default App
+
+const styles : Record<string, CSSProperties>= {
+  wrapper: {
+    width: "100vw",
+    height: "100vh",
+    position:"relative",
+    display: "flex",
+    justifyContent:"center"
+  },
+  chat: {
+    // width: "50%",
+    // padding:"1rem",
+    height: "100%",
+    position: "relative"
+  },
+  header: {
+    position:'sticky',
+    background: "#1f1f1f",
+    zIndex: 1001,
+    height: "2vh",
+    // top: "0",
+    // left: "0"
+  },
+  message: {
+    height: "90vh",
+    overflowX:'hidden',
+    overflowY: 'auto',
+    scrollBehavior: "smooth",
+    
+  },
+  input: {
+  }
+}
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'react-chat-elements/dist/main.css'
 import App from './App.tsx'
-import './index.css'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
