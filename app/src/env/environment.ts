cc5f992ef820862db63bf0bcd53c15027ae49955
nsx07 <felipeaugustodluz@hotmail.com>
2023-09-30 15:28:11 -0300

    corrigindo endpoint

--- app/src/env/environment.ts
+++ app/src/env/environment.ts
@@ -1,5 +1,6 @@
 export const endpoints = {
-    apiHttp: "http://localhost:3000",
-    // apiWs: "wss://bc-server.up.railway.app"
-    apiWs: "ws://localhost:3000"
+    apiWs: "wss://bc-server.up.railway.app",
+    apiHttp: "https://bc-server.up.railway.app",
+    // apiHttp: "http://localhost:3000",
+    // apiWs: "ws://localhost:3000"
 }
